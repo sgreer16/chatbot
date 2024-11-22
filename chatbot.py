250c94fed753ec8b88684a0c39156f3e61b32cf1
sgreer16 <sgreer16@students.kennesaw.edu>
2024-11-21 22:38:03 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,19 @@
 # bot.py
 
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 chatbot = ChatBot("Chatpot")
+
+trainer = ListTrainer(chatbot)
+trainer.train([
+    "Hi",
+    "Welcome, friend 🙃",
+])
+trainer.train([
+    "Are you a plant?",
+    "No, I'm the pot below the plant!",
+])
 
 exit_conditions = (":q", "quit", "exit")
 while True:
